98c10130c6961f48bd5e20ef5a9be2bc5405662d
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-04-09 14:21:35 +0900

    board models 수정
    migrate까지 테스트 완료

--- source/board/models.py
+++ source/board/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class User():
-    title = models.CharField(max_length=100, blank=True)
-    content = models.CharField(max_length=100, blank=True)
+
+class Board(models.Model):
+    class Meta:
+        verbose_name = '게시글'
+        verbose_name_plural = verbose_name
+
+    title = models.CharField('제목', max_length=100, blank=True)
+    content = models.TextField('내용', blank=True)
+
+    def __str__(self):
+        return f'<{self.id}> {self.title}'
+
